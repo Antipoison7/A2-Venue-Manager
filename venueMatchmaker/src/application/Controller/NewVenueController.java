4eb5fbf2637539eb5fb8978a4c509660d9a72db8
Antipoison7 <orders.connor@gmail.com>
2025-02-07 00:11:44 +1100

    Implemented adding venues
    + NewVenueController.java
    + NewVenueModel.java

---
+++ venueMatchmaker/src/application/Controller/NewVenueController.java
@@ -0,0 +1,147 @@
+package application.Controller;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+
+import application.Model.*;
+import application.Model.ObjectClasses.Venue;
+import application.View.ErrorGenerator;
+
+public class NewVenueController {
+
+	//The check Boxes in the Event Types section
+    @FXML
+    private CheckBox checkDisco;
+
+    @FXML
+    private CheckBox checkFestival;
+
+    @FXML
+    private CheckBox checkGig;
+
+    @FXML
+    private CheckBox checkLLC;
+
+    //Submit Button
+    @FXML
+    private Button createVenue;
+
+    //vbox that contains the check boxes, could be useful if the product wants to create events dynamically
+    @FXML
+    private VBox typeVBox;
+
+    //Capacity
+    @FXML
+    private TextField venueCap;
+
+    //Indoor / Outdoor
+    @FXML
+    private ComboBox<String> venueCondition;
+
+    //Name
+    @FXML
+    private TextField venueName;
+
+    //Price Per Hour
+    @FXML
+    private TextField venuePrice;
+    
+    
+    
+    @FXML
+    public void createVenue() 
+    {
+    	try 
+    	{
+    		NewVenueModel venueModel = new NewVenueModel();
+        	
+        	Venue userVenue = new Venue();
+        	
+        	ArrayList<String> suitableTypes = new ArrayList<String>();
+        	
+        	if(checkDisco.isSelected()) 
+        	{
+        		suitableTypes.add("disco");
+        	}
+        	
+        	if(checkFestival.isSelected()) 
+        	{
+        		suitableTypes.add("festival");
+        	}
+        	
+        	if(checkGig.isSelected()) 
+        	{
+        		suitableTypes.add("gig");
+        	}
+        	
+        	if(checkLLC.isSelected()) 
+        	{
+        		System.out.println("Live");
+        		suitableTypes.add("large live concert");
+        	}
+        	
+        	userVenue.setCapacity(Integer.parseInt(venueCap.getText()));
+        	userVenue.setCategory(venueCondition.getValue().toString());
+        	userVenue.setHirePrice(Double.parseDouble(venuePrice.getText()));
+        	userVenue.setName(venueName.getText());
+        	userVenue.setSuitableType(suitableTypes);
+        	userVenue.setId(0);
+        	
+        	if(venueModel.isEntryValid(userVenue)) 
+        	{
+        		if(!venueModel.addNewVenue(userVenue)) 
+        		{
+        			ErrorGenerator errorThrow = new ErrorGenerator();
+        	    	
+        	    	errorThrow.setErrorTitle("DB Error");
+        	    	errorThrow.setErrorBody("Something went wrong, not too sure what, ask your local developer or IT guy.");
+        	    	
+        	    	errorThrow.throwError();
+        		}
+        		else 
+        		{
+        			//TODO: Make this update the bookingManagerView
+        			System.out.println("Window Closed");
+        	    	Stage stage = (Stage) createVenue.getScene().getWindow();
+        	    	stage.close();
+        		}
+        	}
+        	else 
+        	{
+        		ErrorGenerator errorThrow = new ErrorGenerator();
+    	    	
+    	    	errorThrow.setErrorTitle("Invalid values");
+    	    	errorThrow.setErrorBody("One of the fields is invalid, please fix it and try again");
+    	    	
+    	    	errorThrow.throwError();
+        	}
+    	}
+    	catch(Exception e) 
+    	{
+    		ErrorGenerator errorThrow = new ErrorGenerator();
+	    	
+	    	errorThrow.setErrorTitle("Invalid values");
+	    	errorThrow.setErrorBody("One of the fields is invalid, please fix it and try again (e)");
+	    	
+	    	errorThrow.throwError();
+    	}
+    }
+    
+    @FXML
+    public void initialize() 
+    {
+    	ObservableList<String> data = FXCollections.observableArrayList("Indoor","Convertible","Outdoor");
+    	
+    	venueCondition.setItems(data);
+    }
+
+}
---
+++ venueMatchmaker/src/application/Model/NewVenueModel.java
@@ -0,0 +1,99 @@
+package application.Model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import application.Model.ObjectClasses.Venue;
+
+public class NewVenueModel extends JDBCHelper {
+
+	public boolean isEntryValid(Venue userVenue) 
+	{
+		boolean isValid = true;
+		
+		try 
+		{
+			if(userVenue.getCapacity() < 1) 
+			{
+				System.out.println("Small Capacity");
+				isValid = false;
+			}
+			
+			if((!userVenue.getCategory().equals("Indoor"))&&(!userVenue.getCategory().equals("Outdoor"))&&(!userVenue.getCategory().equals("Convertible"))) 
+			{
+				System.out.println("Categories");
+				isValid = false;
+			}
+				
+			if(userVenue.getHirePrice() < 0) 
+			{
+				System.out.println("Hire Price");
+				isValid = false;
+			}
+				
+			if(userVenue.getName().length() <=0) 
+			{
+				System.out.println("Name Length");
+				isValid = false;
+			}
+			
+			if(userVenue.getSuitableType().size()==0) 
+			{
+				System.out.println("Suitable Types");
+				isValid = false;
+			}
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+		
+		return isValid;
+	}
+	
+	public boolean addNewVenue(Venue userVenue) 
+	{
+		boolean isSuccessful = false;
+		
+		try 
+		{
+			Connection jdbc = connectDB();
+			
+			PreparedStatement query;
+			query = jdbc.prepareStatement("INSERT INTO venues (venue_name, hire_price, capacity, category, bookable) VALUES(?,?,?,?,1);");
+			query.setString(1, userVenue.getName());
+			query.setDouble(2, userVenue.getHirePrice());
+			query.setInt(3, userVenue.getCapacity());
+			query.setString(4, userVenue.getCategory());
+			
+			if(query.executeUpdate() !=0) 
+			{
+				isSuccessful = true;
+			}
+			
+			Statement statement = jdbc.createStatement();
+			ResultSet rs = statement.executeQuery("SELECT MAX(venue_id) FROM venues;");
+			
+			int setID = rs.getInt(1);
+			
+			for(String s : userVenue.getSuitableType()) 
+			{
+				PreparedStatement typeQuery;
+				
+				typeQuery = jdbc.prepareStatement("INSERT INTO venues_suitable (venue_id, event_type) VALUES (?,?);");
+				typeQuery.setInt(1, setID);
+				typeQuery.setString(2, s);
+				
+				typeQuery.executeUpdate();
+			}
+		}
+		catch(Exception e) 
+		{
+			return false;
+		}
+		
+		return isSuccessful;
+	}
+}
