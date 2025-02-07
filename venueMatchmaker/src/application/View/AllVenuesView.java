e0b9aafe917ff12b549561d7991ced8ffa7edca0
Antipoison7 <orders.connor@gmail.com>
2025-02-07 17:14:08 +1100

    Created AllVenuesView

---
+++ venueMatchmaker/src/application/View/AllVenuesView.java
@@ -0,0 +1,27 @@
+package application.View;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class AllVenuesView {
+	public void openAllVenues(Stage stage) 
+	{
+		try 
+		{
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("allVenues.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setTitle("All Venues Menu");
+			stage.setResizable(false);
+			stage.setScene(scene);
+			stage.show();
+		}
+		catch(Exception e) 
+		{
+			System.out.println(e);
+		}
+	}
+}
