39c7382ae977da89f34bad7b0b8905bf86f4324f
Antipoison7 <orders.connor@gmail.com>
2025-02-07 12:56:26 +1100

    Create NewUserView.java

---
+++ venueMatchmaker/src/application/View/NewUserView.java
@@ -0,0 +1,27 @@
+package application.View;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class NewUserView {
+	public void openNewUserView(Stage stage) 
+	{
+		try 
+		{
+			VBox root = (VBox)FXMLLoader.load(getClass().getResource("newStaffMember.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setTitle("Employee Manager");
+			stage.setResizable(false);
+			stage.setScene(scene);
+			stage.show();
+		}
+		catch(Exception e) 
+		{
+			System.out.println(e);
+		}
+	}
+}
