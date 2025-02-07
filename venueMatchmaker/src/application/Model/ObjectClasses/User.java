176143685680575ea965e4518335b378fe2e9e87
Antipoison7 <orders.connor@gmail.com>
2025-02-07 12:54:35 +1100

    Created user class

--- venueMatchmaker/src/application/Model/ObjectClasses/User.java
+++ venueMatchmaker/src/application/Model/ObjectClasses/User.java
@@ -1,5 +1,50 @@
 package application.Model.ObjectClasses;
 
 public class User {
+	private String username;
+	private String password;
+	private String realName;
+	private int security;
+	
+	public User() 
+	{
+		
+	}
+
+	public User(int security) {
+		this.security = security;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRealName() {
+		return realName;
+	}
+
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+
+	public int getSecurity() {
+		return security;
+	}
+
+	public void setSecurity(int security) {
+		this.security = security;
+	}
 
 }
