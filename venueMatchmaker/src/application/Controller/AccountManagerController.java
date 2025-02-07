7fae7b917590a6f1a0e3b3ef34460ab47cf99682
Antipoison7 <orders.connor@gmail.com>
2025-02-07 12:52:40 +1100

    Create open new managers and staff

--- venueMatchmaker/src/application/Controller/AccountManagerController.java
+++ venueMatchmaker/src/application/Controller/AccountManagerController.java
@@ -1,17 +1,35 @@
 package application.Controller;
 
+import application.View.BookingManagerView;
+import application.View.EmployeeManagerView;
+import application.View.NewManagerView;
+import application.View.NewUserView;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class AccountManagerController 
 {
+	@FXML
+    private MenuItem ddBookingManager;
+	
+	@FXML
+    private MenuItem ddBackup;
 
     @FXML
-    private Button closeButton;
+    private MenuItem ddCustomise;
 
+    @FXML
+    private MenuItem ddEmployees;
+
+    @FXML
+    private MenuItem ddStats;
+	
     @FXML
     private Button deleteStaff;
 
@@ -39,5 +57,61 @@ public class AccountManagerController
 
     @FXML
     private TextField updateUsername;
+    
+    
+    @FXML
+    public void openNewUser() 
+    {
+    	Stage stage = (Stage) staffTemplate.getScene().getWindow();
+    	NewUserView view = new NewUserView();
+    	view.openNewUserView(stage);
+    }
+    
+    @FXML
+    public void openNewManager() 
+    {
+    	Stage stage = (Stage) staffTemplate.getScene().getWindow();
+    	NewManagerView view = new NewManagerView();
+    	view.openNewUserView(stage);
+    }
+    
+    //Dropdown Methods
+    @FXML
+    public void openBookingManager(ActionEvent e) 
+    {
+    	System.out.println("Open Booking Manager");
+    	
+    	Stage stage = (Stage) deleteStaff.getScene().getWindow();
+    	
+    	BookingManagerView view = new BookingManagerView();
+    	view.openBookingManager(stage);
+    }
+    
+    @FXML
+    public void openCustomiseProfile(ActionEvent e) 
+    {
+    	System.out.println("Open Customise Profile");
+    }
+    
+    @FXML
+    public void openBackupManager(ActionEvent e) 
+    {
+    	System.out.println("Open Backup Manager");
+    }
+    
+    @FXML
+    public void openManagerStats(ActionEvent e) 
+    {
+    	System.out.println("Open Manager Stats");
+    }
+    
+    @FXML
+    public void openAddEmployees(ActionEvent e) 
+    {
+    	System.out.println("Open Add Employees");
+    	Stage stage = (Stage) deleteStaff.getScene().getWindow();
+    	EmployeeManagerView empl = new EmployeeManagerView();
+    	empl.openManagerView(stage);
+    }
 
 }
