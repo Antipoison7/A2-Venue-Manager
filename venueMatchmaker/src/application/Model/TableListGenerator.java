b84beb089d9a56fbb55e562c867ae74d049ba471
Antipoison7 <orders.connor@gmail.com>
2025-02-07 17:13:14 +1100

    Changed EmployeeManagerModel to use more generic name

--- venueMatchmaker/src/application/Model/TableListGenerator.java
+++ venueMatchmaker/src/application/Model/TableListGenerator.java
@@ -5,10 +5,11 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 import application.Model.ObjectClasses.User;
+import application.Model.ObjectClasses.Venue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-public class EmployeeManagerModel extends JDBCHelper{
+public class TableListGenerator extends JDBCHelper{
 	public ObservableList<User> getUsers()
 	{
 		try 
@@ -38,6 +39,49 @@ public class EmployeeManagerModel extends JDBCHelper{
 			statement.close();
 			
 			return users;
+		}
+		catch(Exception e) 
+		{
+			return null;
+		}
+	}
+	
+	public ObservableList<Venue> getVenues()
+	{
+		try 
+		{
+			Connection jdbc =  connectDB();
+			
+			Statement statement = jdbc.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM venues");
+			
+			ObservableList<Venue> venues = FXCollections.observableArrayList();
+			
+			while(resultSet.next()) 
+			{
+				Venue newVenue = new Venue();
+				newVenue.setId(resultSet.getInt(1));
+				newVenue.setName(resultSet.getString(2));
+				newVenue.setHirePrice(resultSet.getDouble(3));
+				newVenue.setCapacity(resultSet.getInt(4));
+				newVenue.setCategory(resultSet.getString(5));
+				
+				if(resultSet.getInt(6) == 1) 
+				{
+					newVenue.setBookable(true);
+				}
+				else 
+				{
+					newVenue.setBookable(false);
+				}
+				
+				venues.add(newVenue);
+			}
+			
+			jdbc.close();
+			statement.close();
+			
+			return venues;
 		}
 		catch(Exception e) 
 		{
