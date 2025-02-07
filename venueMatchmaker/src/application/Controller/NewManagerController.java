85938053cb4ca7398cb2d5e09ae46af526f24d72
Antipoison7 <orders.connor@gmail.com>
2025-02-07 13:40:30 +1100

    Update NewManagerController.java

--- venueMatchmaker/src/application/Controller/NewManagerController.java
+++ venueMatchmaker/src/application/Controller/NewManagerController.java
@@ -1,6 +1,9 @@
 package application.Controller;
 
+import application.Model.NewStaffMembers;
+import application.Model.ObjectClasses.User;
 import application.View.EmployeeManagerView;
+import application.View.ErrorGenerator;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -37,6 +40,58 @@ public class NewManagerController {
     @FXML
     public void addManager(Event e) 
     {
+    	try 
+    	{
+    		NewStaffMembers staff = new NewStaffMembers();
+    		
+    		User selectedUser = new User();
+    		
+    		int secretCode = Integer.parseInt(secretCEOCode.getText());
+    		
+    		selectedUser.setUsername(newUsername.getText());
+    		selectedUser.setPassword(newPassword.getText());
+    		selectedUser.setRealName(newFullName.getText());
+    		selectedUser.setSecurity(1);
+    		
+    		if(staff.isManagerValid(selectedUser, secretCode)) 
+    		{
+    			if(!staff.addNewStaff(selectedUser, 1)) 
+    			{
+    				ErrorGenerator errorThrow = new ErrorGenerator();
+    				
+    				errorThrow.setErrorTitle("DB Error");
+        	    	errorThrow.setErrorBody("Something went wrong, not too sure what, ask your local developer or IT guy.");
+        	    	
+        	    	errorThrow.throwError();
+    			}
+    			else 
+    			{
+    				Stage stage = (Stage) createAccount.getScene().getWindow();
+    		    	EmployeeManagerView view = new EmployeeManagerView();
+    		    	view.openManagerView(stage);
+    			}
+    			
+    		}
+    		else 
+    		{
+    			ErrorGenerator errorThrow = new ErrorGenerator();
+    	    	
+    	    	errorThrow.setErrorTitle("Entry Error");
+    	    	errorThrow.setErrorBody("Something went wrong, not too sure what, ask your local developer or IT guy. Thing");
+    	    	
+    	    	errorThrow.throwError();
+    		}
+    	}
+    	catch(Exception f) 
+    	{
+    		ErrorGenerator errorThrow = new ErrorGenerator();
+	    	
+	    	errorThrow.setErrorTitle("Something Went Wrong");
+	    	errorThrow.setErrorBody(f.toString());
+	    	
+	    	errorThrow.throwError();
+    	}
+    	
     	
     }
 }
