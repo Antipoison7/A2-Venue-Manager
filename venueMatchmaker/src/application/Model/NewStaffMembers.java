cbea89769f29ec8d31b6e2cda15985fe357535e4
Antipoison7 <orders.connor@gmail.com>
2025-02-08 20:06:29 +1100

    NewStaffMembers can now update staff members

--- venueMatchmaker/src/application/Model/NewStaffMembers.java
+++ venueMatchmaker/src/application/Model/NewStaffMembers.java
@@ -90,6 +90,40 @@ public class NewStaffMembers extends JDBCHelper {
 		return isValid;
 	}
 	
+	public boolean isUserValid(User user) 
+	{
+		boolean isValid = true;
+		
+		try 
+		{
+			if(user.getUsername().length() <=0) 
+			{
+				isValid = false;
+			}
+			
+			if(user.getPassword().length() <= 0)
+			{
+				isValid = false;
+			}
+			
+			if(user.getRealName().length() <= 0) 
+			{
+				isValid = false;
+			}
+			
+			if(user.getSecurity() < 0) 
+			{
+				isValid = false;
+			}
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+		
+		return isValid;
+	}
+	
 	public boolean addNewStaff(User user, int security) 
 	{
 		boolean isSuccessful = false;
@@ -104,6 +138,37 @@ public class NewStaffMembers extends JDBCHelper {
 			query.setString(2, user.getPassword());
 			query.setString(3, user.getRealName());
 			query.setInt(4, security);
+			
+			if(query.executeUpdate() !=0) 
+			{
+				isSuccessful = true;
+			}
+			
+			jdbc.close();
+			query.close();
+		}
+		catch(Exception e) 
+		{
+			return false;
+		}
+		
+		return isSuccessful;
+	}
+	
+	public boolean updateStaffMember(User user) 
+	{
+		boolean isSuccessful = false;
+		
+		try 
+		{
+			Connection jdbc = connectDB();
+			
+			PreparedStatement query;
+			query = jdbc.prepareStatement("UPDATE users SET password = ?, real_name = ?, security = ? WHERE username = ?;");
+			query.setString(1, user.getPassword());
+			query.setString(2, user.getRealName());
+			query.setInt(3, user.getSecurity());
+			query.setString(4, user.getUsername());
 			
 			if(query.executeUpdate() !=0) 
 			{
