d09d1062673e2457f9a3bd139131ffe74b927aad
Antipoison7 <orders.connor@gmail.com>
2025-02-09 17:35:47 +1100

    Create AllEventsView.java

---
+++ venueMatchmaker/src/application/View/AllEventsView.java
@@ -0,0 +1,27 @@
+package application.View;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class AllEventsView {
+	public void openAllEvents(Stage stage) 
+	{
+		try 
+		{
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("allEvents.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setTitle("All Events Menu");
+			stage.setResizable(false);
+			stage.setScene(scene);
+			stage.show();
+		}
+		catch(Exception e) 
+		{
+			System.out.println(e);
+		}
+	}
+}
