3d3fc5b169a309a854e81117b8b89d8c2c917e52
Antipoison7 <orders.connor@gmail.com>
2025-02-09 13:47:24 +1100

    Created Request Storage Class

--- venueMatchmaker/src/application/Model/ObjectClasses/Request.java
+++ venueMatchmaker/src/application/Model/ObjectClasses/Request.java
@@ -1,5 +1,98 @@
 package application.Model.ObjectClasses;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Request {
+	private int requestID;
+	private String clientName;
+	private String title;
+	private String artist;
+	private LocalDate date;
+	private LocalTime time;
+	private Double duration;
+	private int audienceNumber;
+	private String type;
+	private String category;
+
+	public int getRequestID() {
+		return requestID;
+	}
+
+	public void setRequestID(int requestID) {
+		this.requestID = requestID;
+	}
+
+	public String getClientName() {
+		return clientName;
+	}
+
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
+
+	public Double getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Double duration) {
+		this.duration = duration;
+	}
+
+	public int getAudienceNumber() {
+		return audienceNumber;
+	}
+
+	public void setAudienceNumber(int audienceNumber) {
+		this.audienceNumber = audienceNumber;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
 
 }
