e937847e125c253959ec8fccab7d8f5605c27af0
Antipoison7 <orders.connor@gmail.com>
2025-02-12 14:35:54 +1100

    Added colours to BookVenueController

--- venueMatchmaker/src/application/Controller/BookVenueController.java
+++ venueMatchmaker/src/application/Controller/BookVenueController.java
@@ -5,15 +5,19 @@ import java.util.HashMap;
 import application.Model.BookVenueModel;
 import application.Model.ObjectDBInterface;
 import application.Model.ObjectClasses.Request;
+import application.View.BookingManagerView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class BookVenueController {
 	
 	private static String venueName;
 	private static int requestID;
+	private static boolean groupBooking;
 
     @FXML
     private Button bookVenue;
@@ -37,13 +41,27 @@ public class BookVenueController {
     private Label compType;
 
     @FXML
-    void bookVenue(ActionEvent event) {
-
+    void bookVenue(ActionEvent event) 
+    {
+    	ObjectDBInterface db = new ObjectDBInterface();
+    	BookVenueModel book = new BookVenueModel();
+    	
+    	try 
+    	{
+    		
+    	}
+    	catch(Exception e) 
+    	{
+    		
+    	}
     }
 
     @FXML
     void discard(ActionEvent event) {
-
+    	Stage stage = (Stage) buttonDiscard.getScene().getWindow();
+    	
+    	BookingManagerView view = new BookingManagerView();
+    	view.openBookingManager(stage);
     }
 
     public void initialize() 
@@ -52,45 +70,54 @@ public class BookVenueController {
     	
     	BookVenueModel book = new BookVenueModel();
     	
-    	HashMap<String, Boolean> compare = book.calculateCompatibility(db.selectRequest(requestID), db.selectVenueNoType(venueName));
+    	HashMap<String, Boolean> compare = book.calculateCompatibility(db.selectRequest(requestID), db.selectVenue(venueName));
     	
     	if(compare.get("Capacity")) 
     	{
     		compCapacity.setText("Enough Capacity");
+    		compCapacity.setTextFill(Color.GREEN);
     	}
     	else 
     	{
     		compCapacity.setText("Not Enough Capacity");
+    		compCapacity.setTextFill(Color.RED);
     	}
     	
     	if(compare.get("Type")) 
     	{
     		compType.setText("Perfect Conditions");
+    		compType.setTextFill(Color.GREEN);
     	}
     	else 
     	{
     		compType.setText("Not Perfect Conditions");
+    		compType.setTextFill(Color.RED);
     	}
     	
     	if(compare.get("Event")) 
     	{
     		compEvent.setText("Perfect Match");
+    		compEvent.setTextFill(Color.GREEN);
     	}
     	else 
     	{
     		compEvent.setText("Not Match");
+    		compEvent.setTextFill(Color.RED);
     	}
     	
     	if(compare.get("Double")) 
     	{
     		compDoubleBooking.setText("Not Double Booked");
+    		compDoubleBooking.setTextFill(Color.GREEN);
     	}
     	else 
     	{
     		compDoubleBooking.setText("Double Booked");
+    		compDoubleBooking.setTextFill(Color.RED);
+    		bookVenue.setDisable(true);
     	}
     	
-    	compScore.setText("" + (book.getCompatibilityNumber(db.selectRequest(requestID), db.selectVenueNoType(venueName))*25));
+    	compScore.setText((book.getCompatibilityNumber(db.selectRequest(requestID), db.selectVenue(venueName))*25) + "%");
     	
 //    	Request selectedRequest = db.selectRequest(ID);
     }
@@ -103,5 +130,10 @@ public class BookVenueController {
     public static void setRequestID(int id) 
     {
     	requestID = id;
+    }
+    
+    public static void setGroupBooking(boolean group) 
+    {
+    	groupBooking = group;
     }
 }
