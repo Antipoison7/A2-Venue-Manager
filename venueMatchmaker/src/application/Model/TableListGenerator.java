d6c1b04067e17a7ae8c92dbd65e768d35faf0944
Antipoison7 <orders.connor@gmail.com>
2025-02-12 14:32:09 +1100

    Can now generate bookings using TableListGenerator

--- venueMatchmaker/src/application/Model/TableListGenerator.java
+++ venueMatchmaker/src/application/Model/TableListGenerator.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.HashMap;
 
+import application.Model.ObjectClasses.Booking;
 import application.Model.ObjectClasses.Request;
 import application.Model.ObjectClasses.User;
 import application.Model.ObjectClasses.Venue;
@@ -176,6 +177,101 @@ public class TableListGenerator extends JDBCHelper{
 		}
 		catch(Exception e) 
 		{
+			return null;
+		}
+	}
+	
+	public ObservableList<Booking> getEvents()
+	{
+		try 
+		{
+			Connection jdbc =  connectDB();
+			
+			Statement statement = jdbc.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM event_bookings");
+			
+			ObservableList<Booking> bookings = FXCollections.observableArrayList();
+			
+			while(resultSet.next()) 
+			{
+				Booking newBooking = new Booking();
+				newBooking.setRequestID(resultSet.getInt(1));
+				newBooking.setClientName(resultSet.getString(2));
+				newBooking.setStaff(resultSet.getString(3));
+				newBooking.setCost(resultSet.getDouble(4));
+				newBooking.setCommission(resultSet.getDouble(5));
+				newBooking.setTitle(resultSet.getString(6));
+				newBooking.setArtist(resultSet.getString(7));
+				newBooking.setDate(resultSet.getString(8));
+				newBooking.setTime(resultSet.getString(9));
+				newBooking.setDuration(resultSet.getDouble(10));
+				newBooking.setAudienceNumber(resultSet.getInt(11));
+				newBooking.setType(resultSet.getString(12));
+				newBooking.setGroup(resultSet.getBoolean(13));
+				newBooking.setCategory(resultSet.getString(14));
+				newBooking.setVenue(resultSet.getString(15));
+				
+				bookings.add(newBooking);
+			}
+			
+			jdbc.close();
+			statement.close();
+			
+			return bookings;
+		}
+		catch(Exception e) 
+		{
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	public ObservableList<Booking> getEventsVenue(String s)
+	{
+		try 
+		{
+			Connection jdbc =  connectDB();
+			
+			PreparedStatement query;
+			
+			query = jdbc.prepareStatement("SELECT * FROM event_bookings WHERE venue = ?");
+			
+			query.setString(1, s);
+			
+			ResultSet resultSet = query.executeQuery();
+			
+			ObservableList<Booking> bookings = FXCollections.observableArrayList();
+			
+			while(resultSet.next()) 
+			{
+				Booking newBooking = new Booking();
+				newBooking.setRequestID(resultSet.getInt(1));
+				newBooking.setClientName(resultSet.getString(2));
+				newBooking.setStaff(resultSet.getString(3));
+				newBooking.setCost(resultSet.getDouble(4));
+				newBooking.setCommission(resultSet.getDouble(5));
+				newBooking.setTitle(resultSet.getString(6));
+				newBooking.setArtist(resultSet.getString(7));
+				newBooking.setDate(resultSet.getString(8));
+				newBooking.setTime(resultSet.getString(9));
+				newBooking.setDuration(resultSet.getDouble(10));
+				newBooking.setAudienceNumber(resultSet.getInt(11));
+				newBooking.setType(resultSet.getString(12));
+				newBooking.setGroup(resultSet.getBoolean(13));
+				newBooking.setCategory(resultSet.getString(14));
+				newBooking.setVenue(resultSet.getString(15));
+				
+				bookings.add(newBooking);
+			}
+			
+			jdbc.close();
+			query.close();
+			
+			return bookings;
+		}
+		catch(Exception e) 
+		{
+			System.out.println(e);
 			return null;
 		}
 	}
